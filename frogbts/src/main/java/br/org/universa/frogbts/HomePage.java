4b1990f8e0e59f1a18ec87ce09d45959ce145ea9
Denis Oliveira <denisbito@gmail.com>
2010-10-17 20:50:36 -0200

    modificada homepage do projeto para exibir nome do sistema

--- frogbts/src/main/java/br/org/universa/frogbts/HomePage.java
+++ frogbts/src/main/java/br/org/universa/frogbts/HomePage.java
@@ -1,8 +1,8 @@
 package br.org.universa.frogbts;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 
 /**
  * Homepage
@@ -13,17 +13,17 @@ public class HomePage extends WebPage {
 
 	// TODO Add any page properties or variables here
 
-    /**
+	/**
 	 * Constructor that is invoked when page is invoked without a session.
 	 * 
 	 * @param parameters
 	 *            Page parameters
 	 */
-    public HomePage(final PageParameters parameters) {
+	public HomePage(final PageParameters parameters) {
 
-        // Add the simplest type of label
-        add(new Label("message", "If you see this message wicket is properly configured and running"));
+		// Add the simplest type of label
+		add(new Label("message", "primeira versão do FrogBTS."));
 
-        // TODO Add your page's components here
-    }
+		// TODO Add your page's components here
+	}
 }
